c00027578d54e4440c2e0f41c7ed90aee9b87a68
carlosgsouza <carlosgsouza@gmail.com>
2021-12-08 22:44:23 -0800

    Introducing a Tree object that acts as a builder when invoked

--- src/test/kotlin/carlosgsouza/learningkotlin/builders/TreeBuilder.kt
+++ src/test/kotlin/carlosgsouza/learningkotlin/builders/TreeBuilder.kt
@@ -5,26 +5,42 @@ internal class TreeBuilderTest {
 
     @Test
     fun test() {
-        val t = node(1) {
-            left = node(21)
-            right = node(22) {
-                right = node(222)
+        val t = Tree {
+            node(1) {
+                left = node(21)
+                right = node(22) {
+                    right = node(222)
+                }
             }
         }
 
-        assertEquals(  1, t.value)
-        assertEquals( 21, t.left?.value)
-        assertEquals( 22, t.right?.value)
-        assertEquals( 222, t.right?.right?.value)
+        assertEquals(  1, t.root?.value)
+        assertEquals( 21, t.root?.left?.value)
+        assertEquals( 22, t.root?.right?.value)
+        assertEquals( 222, t.root?.right?.right?.value)
 
     }
 
-    data class Node(val value: Int, var left: Node? = null, var right: Node? = null)
+    data class Tree(var root: Node? = null) {
+        companion object {
+            operator fun invoke(buildFun: Tree.() -> Unit): Tree = Tree().apply(buildFun)
+        }
 
-    fun node(value: Int, build: (Node.() -> Unit)? = null): Node {
-        val node = Node(value)
-        if(build != null) node.apply(build)
-        return node
+        fun node(value: Int, buildFun: (Node.() -> Unit)?): Node {
+            val r = Node(value)
+            if(buildFun != null) r.apply(buildFun)
+            root = r
+            return r
+        }
+    }
+
+
+    data class Node(val value: Int, var left: Node? = null, var right: Node? = null) {
+        fun node(value: Int, buildFun: (Node.() -> Unit)? = null): Node {
+            val node = Node(value)
+            if (buildFun != null) node.apply(buildFun)
+            return node
+        }
     }
 }
 
